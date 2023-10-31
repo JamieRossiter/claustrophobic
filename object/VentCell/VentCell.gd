74ebaaf04e14cb94ab1a56462626c583b35a50f1
Jamie Rossiter <jamieprossiter@gmail.com>
2023-10-31 11:11:03 +1100

    Vent cells are procedurally created - Vent is comprised of multiple vent "sections" that represent floor, ceiling and walls - Vent sections and their respective meshes and collision shapes are procedurally:     - Sized (x,y)     - Rotated     - Positioned (x,y,z)

---
+++ object/VentCell/VentCell.gd
@@ -0,0 +1,73 @@
+@tool
+class_name VentCell extends Node3D
+
+const VentSection: PackedScene = preload("res://object/VentCell/VentSection.tscn");
+
+@onready var north_wall: VentSection = VentSection.instantiate();
+@onready var south_wall: VentSection = VentSection.instantiate();
+@onready var east_wall: VentSection = VentSection.instantiate()
+@onready var west_wall: VentSection = VentSection.instantiate()
+@onready var ceiling: VentSection = VentSection.instantiate()
+@onready var floor: VentSection = VentSection.instantiate()
+
+const FLOOR_WIDTH: float = 0.6;
+const WALL_HEIGHT: float = 0.3;
+
+func _ready() -> void:
+	add_sections_to_tree();
+	setup_sections();
+
+func add_sections_to_tree() -> void:
+	add_child(north_wall);
+	add_child(south_wall);
+	add_child(east_wall);
+	add_child(west_wall);
+	add_child(floor);
+	add_child(ceiling);
+
+func setup_sections() -> void:
+	setup_floor_ceiling(floor, false);
+	setup_floor_ceiling(ceiling, true);
+	setup_wall(north_wall, "north");
+	setup_wall(south_wall, "south");
+	setup_wall(east_wall, "east");
+	setup_wall(west_wall, "west");
+
+func setup_floor_ceiling(section: VentSection, is_ceiling: bool) -> void:
+	# Configure mesh
+	var meshInstance: MeshInstance3D = section.get_node("MeshInstance3D");
+	var mesh: PlaneMesh = meshInstance.mesh;
+	mesh.size.x = FLOOR_WIDTH;
+	if(is_ceiling):
+		section.position.y = WALL_HEIGHT;
+		mesh.flip_faces = true;
+
+func setup_wall(wall: VentSection, direction: String) -> void:
+	# Configure mesh
+	var meshInstance: MeshInstance3D = wall.get_node("MeshInstance3D");
+	var mesh: PlaneMesh = meshInstance.mesh;
+	mesh.size.y = WALL_HEIGHT;
+	# Configure static body
+	wall.rotate_x(deg_to_rad(90));
+	wall.position.y = mesh.size.y / 2;
+	match(direction):
+		"north":
+			wall.position.z = -0.5;
+			mesh.size.x = FLOOR_WIDTH;
+		"south":
+			wall.position.z = 0.5; 
+			mesh.size.x = FLOOR_WIDTH;
+			mesh.flip_faces = true;
+		"east":
+			wall.position.x = FLOOR_WIDTH / 2;
+			wall.rotate_y(deg_to_rad(90));
+			mesh.flip_faces = true;
+			mesh.subdivide_depth = 100;
+			mesh.subdivide_width = 100;
+		"west":
+			wall.position.x = -FLOOR_WIDTH / 2;
+			wall.rotate_y(deg_to_rad(90));
+	# Configure collision
+	var collisionShape: CollisionShape3D = wall.get_node("CollisionShape3D");
+	collisionShape.get_shape().set_size(Vector3(mesh.size.x, mesh.size.y, 0));
+	collisionShape.rotate_x(deg_to_rad(90));
---
+++ object/VentCell/VentSection.gd
@@ -0,0 +1,2 @@
+class_name VentSection extends StaticBody3D
+
